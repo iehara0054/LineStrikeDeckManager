39e5649695f88f7e41b1483391513f12e1979821
iehara0054 <iehara0054@gmail.com>
2026-02-18 12:19:38 +0900

    CSRF対策追加

--- API/auth/signup_api.php
+++ API/auth/signup_api.php
@@ -1,4 +1,16 @@
 <?php
-$mail = $_POST['mail'];
-$pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
-$password_confirm = password_hash($_POST['password_confirm'], PASSWORD_DEFAULT);
+$errors = [];
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
+{
+    if (!hash_equals($_SESSION["csrf_token"], $_POST['csrf_token'] ?? ''))
+    {
+        http_response_code(400);
+        $errors[] = 'Invalid CSRF token.';
+    }
+    else
+    {
+        $mail = $_POST['mail'];
+        $pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
+        $password_confirm = password_hash($_POST['password_confirm'], PASSWORD_DEFAULT);
+    }
+}
