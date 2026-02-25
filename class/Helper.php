12efa2bbdd67c92f6b78cac135bf9a4507c9477c
iehara0054 <iehara0054@gmail.com>
2026-02-25 11:46:54 +0900

    ヘルパーにURLバリデーション追加

--- class/Helper.php
+++ class/Helper.php
@@ -4,4 +4,15 @@ class Helper
     // ========================================
     // 定数定義とヘルパー関数
     // ========================================
+
+    /**
+     * URLのバリデーション
+     *
+     * @param string $url バリデーションする文字列
+     * @param bool バリデーションの結果
+     */
+    public function is_valid_url(string $url): bool
+    {
+        return filter_var($url, FILTER_VALIDATE_URL) !== false && preg_match('@^https?+://@i', $url) > 0;
+    }
 }
